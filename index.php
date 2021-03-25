1329675dfd097095f9dfa668cdf1446c59fe47a3
Nastasya.Churashova <gregarious@list.ru>
2021-03-25 09:34:24 +0200

    function increase created

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 <?php
 
+include_once 'Controller.php';
+$manager = new Controller();//создаём новый объект из класса Controller
+
 $amount = array_key_exists('amount', $_GET) ? $_GET['amount'] : 42;
 $amount = 42;
 
@@ -10,12 +13,12 @@ if (array_key_exists('amount', $_GET)) {
         $amount= $_GET['amount'];
     }
     $amount = $_GET['amount'];
-    $content = json_encode(['amount' => $amount]);
+    $content = json_encode(['amount' => $amount, 'links' => []]);
 
     file_put_contents('db.json', $content);
 } else {
     if (file_exists('db.json')) {
-        $db = json_decode(file_get_contents('db.json'), true);
+        $db = json_decode(file_get_contents('db.json'), true);//true - чтобы db был массивом, а не объектом
         $amount = $db['amount']; // берём не весь объект,а с ключом amount
     } 
 }
@@ -23,25 +26,34 @@ if (array_key_exists('amount', $_GET)) {
 <form action="" method="get">
     <input type="number" name="amount" value="<?= $amount; ?>">
     <button type="submit">submit</button>
-</form>
-
+</form> 
 
 <div class="container">
     <?php
 
-
- 
-for ($i = 1; $i <= $amount; $i++) {
-    $classes= ($i % 2 == 0) ? "class='new_colour" : '';
-        echo "<a $classes href='?id=$i'>$i </a>";
-        }
-// $links = json_encode('db.json', JSON_PRETTY_PRINT);
-//file_put_contents($id, $links);
+$new_value='';
+$id='';
 
 if (array_key_exists('id', $_GET)){
-    if ($_GET['id'] % 3 === 0){
+    if ($_GET['id'] % 3 == 0){
         echo $_GET['id'];
+        // $new_value=$_GET['id'] +1;
+        // $manager ->add($_GET['id'], $new_value);
+        $manager ->increase($_GET['id']);
     }
 }
-    ?>
+
+for ($id = 1; $id <= $amount; $id++) {
+    $classes= ($id % 2 === 0) ? "class='new_colour'" : '';
+    if (array_key_exists($id, $manager->getAll())){
+        $value = $manager->getAll()[$id];
+    }
+    else {
+        $value= $id;
+    }
+    echo "<a $classes href='?id=$id'>$value </a>";
+}
+
+
+?>
 </div>
